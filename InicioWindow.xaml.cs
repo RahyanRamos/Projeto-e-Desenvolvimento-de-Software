bc613a03e6706ee5c1384437d8f83a348c60cc32
Rahyan Ramos <rahyanramos@gmail.com>
2022-04-02 16:23:45 -0400

    Adicionar arquivos de projeto.

---
+++ InicioWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Atividade1e2
+{
+    /// <summary>
+    /// Lógica interna para InicioWindow.xaml
+    /// </summary>
+    public partial class InicioWindow : Window
+    {
+        public InicioWindow()
+        {
+            InitializeComponent();
+            Loaded += InicioWindow_Loaded;
+        }
+
+        private void InicioWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            txtAltura.Focus();
+        }
+
+        private void txtAltura_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                txtPeso.Focus();
+            }
+        }
+
+        private void txtPeso_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                btCalcular_Click(sender, e);
+            }
+        }
+
+        private void btCalcular_Click(object sender, RoutedEventArgs e)
+        {
+            double resultado, peso, altura;
+
+            peso = Convert.ToDouble(txtPeso.Text);
+            altura = Convert.ToDouble(txtAltura.Text);
+
+            resultado = peso / (altura * altura);
+
+            txtResultado.Text = resultado.ToString();
+
+            if(resultado < 18.5)
+            {
+                MessageBox.Show("Abaixo do peso", "Resultado", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            if(resultado >= 18.5 && resultado <= 24.9)
+            {
+                MessageBox.Show("Peso normal", "Resultado", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            if(resultado >= 25 && resultado <= 29.9)
+            {
+                MessageBox.Show("Sobrepeso", "Resultado", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            if(resultado >= 30 && resultado <= 34.9)
+            {
+                MessageBox.Show("Obesidade grau 1", "Resultado", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            if(resultado >= 35 && resultado <= 39.9)
+            {
+                MessageBox.Show("Obesidade grau 2", "Resultado", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            if(resultado >= 40)
+            {
+                MessageBox.Show("Obesidade grau 3 ou mórbida", "Resultado", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
+            txtAltura.Clear();
+            txtPeso.Clear();
+            txtResultado.Clear();
+            txtAltura.Focus();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Atividade1e2
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            txtAcesso.Focus();
+        }
+
+        private void txtAcesso_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                pwSenha.Focus();
+            }
+        }
+
+        private void pwSenha_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                Button_Click(sender, e);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string senha = pwSenha.Password.ToString();
+            string acesso = txtAcesso.Text;
+
+            if (acesso == "João" && senha == "joao")
+            {
+                MessageBox.Show("Acesso feito com sucesso!");
+                InicioWindow inicio = new InicioWindow();
+                inicio.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Erro no Login!", "ERRO", MessageBoxButton.OK, MessageBoxImage.Error);
+                pwSenha.Clear();
+                txtAcesso.Clear();
+                txtAcesso.Focus();
+            }
+        }
+    }
+}
